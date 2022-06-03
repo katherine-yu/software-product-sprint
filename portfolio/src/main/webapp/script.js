bc991f8599d57f3ff186d598d48a6dfc9aa305c7
Katherine Yu <katherineyangyu@gmail.com>
2022-06-03 12:49:25 +0000

    Edit to personalize index.html, script.js, and style.css.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,13 +16,83 @@
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+  const facts =
+      ["I'm left-handed.", "My right eye has 20/20 vision, while my left eye has 20/100 vision.", "My favorite flavor of ice cream is mint chocolate chip.", "I like to listen to the Beatles.", "My favorite color is purple.", "I want to visit Antarctica someday.", "I like to watch let's play videos of horror games."];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const fact = facts[Math.floor(Math.random() * facts.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const factContainer = document.getElementById('random-fact-container');
+  factContainer.innerText = fact;
+}
+
+/* Shows the previous image in the gallery. */
+function previousImg() {
+    const images = ["images/ico1.jpg", "images/ico2.jpg", "images/fountain.jpg", "images/palm-tree.jpg", "images/ice-cream.jpeg", "images/sunset.jpg"];
+
+    idx = parseInt(document.querySelector('#galleryimg').dataset.idx);
+    prev_idx = (idx - 1 + images.length) % images.length;
+    document.querySelector('#galleryimg').src = images[prev_idx];
+    document.querySelector('#galleryimg').dataset.idx = prev_idx;
+}
+
+/* Shows the next image in the gallery. */
+function nextImg() {
+    const images = ["images/ico1.jpg", "images/ico2.jpg", "images/fountain.jpg", "images/palm-tree.jpg", "images/ice-cream.jpeg", "images/sunset.jpg"];
+
+    idx = parseInt(document.querySelector('#galleryimg').dataset.idx);
+    console.log(idx + 1)
+    next_idx = (idx + 1) % images.length;
+    document.querySelector('#galleryimg').src = images[next_idx];
+    document.querySelector('#galleryimg').dataset.idx = next_idx;
+}
+
+/* Shows or hides the Mandelbrot fractal. */
+function selectMandelbrot() {
+    if (document.querySelector('#fractal').src != "https://upload.wikimedia.org/wikipedia/commons/a/a4/Mandelbrot_sequence_new.gif")
+    {
+        document.querySelector('#fractal').src = "https://upload.wikimedia.org/wikipedia/commons/a/a4/Mandelbrot_sequence_new.gif";
+        document.querySelector('#fractal').alt = "Mandelbrot set";
+        document.querySelector('#fractal').style.visibility = "visible";
+    }
+    else
+    {
+        hideFractal();
+    }
+}
+
+/* Shows or hides the Sierpinski fractal. */
+function selectSierpinski() {
+    if (document.querySelector('#fractal').src != "https://upload.wikimedia.org/wikipedia/commons/a/a9/Fractal_tree.gif")
+    {
+        document.querySelector('#fractal').src = "https://upload.wikimedia.org/wikipedia/commons/a/a9/Fractal_tree.gif";
+        document.querySelector('#fractal').alt = "Sierpinski gasket";
+        document.querySelector('#fractal').style.visibility = "visible";
+    }
+    else
+    {
+        hideFractal();
+    }
+}
+
+/* Shows or hides the Koch fractal. */
+function selectKoch() {
+    if (document.querySelector('#fractal').src != "https://upload.wikimedia.org/wikipedia/commons/f/fd/Von_Koch_curve.gif")
+    {
+        document.querySelector('#fractal').src = "https://upload.wikimedia.org/wikipedia/commons/f/fd/Von_Koch_curve.gif";
+        document.querySelector('#fractal').alt = "Koch snowflake";
+        document.querySelector('#fractal').style.visibility = "visible";
+    }
+    else
+    {
+        hideFractal();
+    }
+}
+
+/* Hides the fractal. */
+function hideFractal() {
+    document.querySelector('#fractal').style.visibility = "hidden";
+    document.querySelector('#fractal').src = "";
+    document.querySelector('#fractal').alt = "";
 }
